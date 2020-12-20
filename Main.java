11befda5a7568de015802708518d6b0208575d59
Naito TAKERU <e205720@ie.u-ryukyu.ac.jp>
2020-12-20 17:13:05 +0900

    3st commit

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.print(e.getMessage());
-        }
+            
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
